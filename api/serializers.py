29310aed9961c844ccb99bb62326b53d3e326ce3
Clinton Dreisbach <clinton@dreisbach.us>
2020-06-23 16:39:20 -0400

    Nested model serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from users.models import User
-from recipes.models import Recipe
+from recipes.models import Recipe, Ingredient
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -9,7 +9,35 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['id', 'url', 'username', 'email', 'is_staff']
 
 
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['amount', 'item']
+
+
 class RecipeSerializer(serializers.HyperlinkedModelSerializer):
+    ingredients = IngredientSerializer(many=True, required=False)
+    user = serializers.StringRelatedField()
+
+    def create(self, validated_data):
+        ingredients = validated_data.pop('ingredients', [])
+        recipe = Recipe.objects.create(**validated_data)
+        for ingredient in ingredients:
+            recipe.ingredients.create(**ingredient)
+        return recipe
+
+    def update(self, instance, validated_data):
+        recipe = instance
+        ingredients = validated_data.pop('ingredients', [])
+        for key, value in validated_data.items():
+            setattr(recipe, key, value)
+        recipe.save()
+
+        recipe.ingredients.all().delete()
+        for ingredient in ingredients:
+            recipe.ingredients.create(**ingredient)
+        return recipe
+
     class Meta:
         model = Recipe
         fields = [
@@ -19,4 +47,5 @@ class RecipeSerializer(serializers.HyperlinkedModelSerializer):
             'prep_time_in_minutes',
             'user',
             'public',
+            'ingredients',
         ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from users.models import User
-from recipes.models import Recipe, get_available_recipes_for_user
-from api.serializers import UserSerializer, RecipeSerializer
+from recipes.models import Recipe, get_available_recipes_for_user, Ingredient
+from api.serializers import UserSerializer, RecipeSerializer, IngredientSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -18,3 +18,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
         queryset = get_available_recipes_for_user(Recipe.objects.all(),
                                                   self.request.user)
         return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
