f68dedfb9221071b4fa7ece99c3d88fdc37243be
Clinton Dreisbach <clinton@dreisbach.us>
2020-06-29 21:02:45 -0400

    Add starting tests

--- recipes/models.py
+++ recipes/models.py
@@ -82,6 +82,11 @@ class Recipe(models.Model):
             tags.append(tag)
         self.tags.set(tags)
 
+    def total_time_in_minutes(self):
+        if self.cook_time_in_minutes is None or self.prep_time_in_minutes is None:
+            return None
+        return self.cook_time_in_minutes + self.prep_time_in_minutes
+
     def __str__(self):
         return self.title
 
