f65c54084b078205dcb4bca58f39cdef18e6d293
Clinton Dreisbach <clinton@dreisbach.us>
2020-07-13 14:44:58 -0400

    Update for Heroku

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -165,7 +165,7 @@ REST_FRAMEWORK = {
     ]
 }
 
-# Django-Heroku setup
+# Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
 del DATABASES['default']['OPTIONS']['sslmode']
