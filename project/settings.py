a6e9ec42d6046dc27bbcb647120ba37951e60b3c
Clinton Dreisbach <clinton@dreisbach.us>
2020-07-30 15:08:32 -0400

    Use S3 for user media

--- project/settings.py
+++ project/settings.py
@@ -18,6 +18,7 @@ import environ
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False),
+    USE_S3=(bool, False),
 )
 environ.Env.read_env()
 
@@ -57,6 +58,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "djoser",
     "imagekit",
+    "storages",
     # Project-specific
     "users",
     "recipes",
@@ -157,6 +159,17 @@ REST_FRAMEWORK = {
     ],
 }
 
+# AWS configuration
+
+if env("USE_S3"):
+    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_OBJECT_PARAMETERS = {
+        "CacheControl": "max-age=86400",
+    }
+    DEFAULT_FILE_STORAGE = "project.storage_backends.MediaStorage"
 
 # Configure Django App for Heroku.
 import django_heroku
